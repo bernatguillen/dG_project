9410aebd36a4a87253a3687c2f66139fc6f37800
Bernat Guillén <bguillen@gmail.com>
2015-05-10 19:08:22 -0400

    Unittest
    Tests for JacobiGQ and GL

--- unittest.cc
+++ unittest.cc
@@ -38,11 +38,49 @@ int TestJacobiP(){
   return 0;
 }
 
+int TestJacobiGQ(){
+  const int N=4;
+  double x[N+1];
+  double w[N+1];
+  double xref[] = {0.830223896278566929872,0.4688487934707142138038,0,-0.830223896278566929872,-0.4688487934707142138038};
+  double wref[] = {0.086017682122807453242,0.336839460734335403901,0.487619047619047619048,0.086017682122807453242,0.336839460734335403901};
+
+  JacobiGQ(N,1,1,x,w);
+  for (int i = 0; i < N+ 1; ++i){
+    std::cout<<"(xi,wi): " << x[i] << " " << w[i] <<std::endl;
+    if(abs(xref[i]-x[i])>1e-8 || abs(wref[i]-w[i])>1e-8) return 1;
+  }
+
+  return 0;
+}
+
+int TestJacobiGL(){
+  const int N = 4;
+  double x[N+1];
+  double xref[] = {-1,0.6546536707079771437983,0,-0.6546536707079771437983,1};
+  JacobiGL(N,0,0,x);
+  for (int i = 0; i < N+1; ++i){
+    std::cout<<"xi :"<<x[i]<<std::endl;
+    if(abs(xref[i]-x[i])>1e-8) return 1;
+  }
+  return 0;
+}
 int main(int argc, char **argv){
   if(!TestJacobiP()){
     std::cout<<"TestJacobiP passed!"<<std::endl;
   }else{
     std::cout<<"TestJacobiP failed!"<<std::endl;
   }
+  if(!TestJacobiGQ()){
+    std::cout<<"TestJacobiGQ passed!"<<std::endl;
+  }else{
+    std::cout<<"TestJacobiGQ failed!"<<std::endl;
+  }
+    if(!TestJacobiGL()){
+    std::cout<<"TestJacobiGL passed!"<<std::endl;
+  }else{
+    std::cout<<"TestJacobiGL failed!"<<std::endl;
+  }
+
   return(1);
 }
